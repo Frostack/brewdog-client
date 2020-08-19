97d5ac0e824f624c7b5c39f80e7bab49f5fa1b29
Frostack <soulburn007@gmail.com>
2020-08-19 21:03:58 +0430

    🏷 add sortTypes

---
+++ src/constants/sortTypes.js
@@ -0,0 +1,5 @@
+export const DEFAULT = 'DEFAULT';
+export const ABV_TO_LOW = 'ABV_TO_LOW';
+export const ABV_TO_HIGH = 'ABV_TO_HIGH';
+export const NAME_ASCENDING = 'NAME_ASCENDING';
+export const NAME_DESCENDING = 'NAME_DESCENDING';
