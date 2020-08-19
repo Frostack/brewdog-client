9d925a282d0ec52cbd4e7a92d6c7b749ac841c4c
Frostack <soulburn007@gmail.com>
2020-08-19 21:01:58 +0430

    🧱 add CustomModal

---
+++ src/components/UI/Modal/CustomModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+function CustomModal({ children, show, onHide, title, tagline }) {
+  return (
+    <Modal size="lg" show={show} centered onHide={onHide}>
+      <Modal.Header closeButton className="bg-light d-flex align-items-center px-4">
+        <Modal.Title>{title}</Modal.Title>
+        <div className="ml-5 text-secondary">{tagline}</div>
+      </Modal.Header>
+
+      <Modal.Body className="p-0">{children}</Modal.Body>
+    </Modal>
+  );
+}
+
+export default CustomModal;
