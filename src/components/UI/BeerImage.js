3880091aece20505544a528890fa41a352739dfb
Frostack <soulburn007@gmail.com>
2020-08-19 21:01:09 +0430

    🧱 add BeerImage

---
+++ src/components/UI/BeerImage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BeerImage({ src, alt, width, height }) {
+  return <img src={src} alt={alt} className="image" width={width} height={height} />;
+}
+
+export default BeerImage;
