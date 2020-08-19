ad4b8d19fa8ccf601a91e3890e85b50b15e43dda
Frostack <soulburn007@gmail.com>
2020-08-19 21:00:38 +0430

    🧱 add CartList

---
+++ src/components/CartScreen/CartList/CartList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CartItem from './CartItem';
+
+function CartList({ items }) {
+  const renderItems = () => {
+    return items.map(beer => <CartItem key={beer.id} data={beer} />);
+  };
+
+  return (
+    <div className="h-100" style={{ contain: 'size', overflowY: 'scroll' }}>
+      {renderItems()}
+    </div>
+  );
+}
+
+export default CartList;
