5e4880bcf9e4cb0e0690a0ad76c9d608af6916fb
Frostack <soulburn007@gmail.com>
2020-08-19 21:02:32 +0430

    🎭 add modalAction

---
+++ src/actions/modalActions.js
@@ -0,0 +1,17 @@
+import * as types from '../constants/actionTypes';
+
+export const showModal = data => {
+  return { type: types.MODAL_SHOW, data };
+};
+
+export const hideModal = () => {
+  return { type: types.MODAL_HIDE };
+};
+
+export const showError = msg => {
+  return { type: types.ERROR_SHOW, msg };
+};
+
+export const hideError = () => {
+  return { type: types.ERROR_HIDE };
+};
