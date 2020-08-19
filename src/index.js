496ae4059d4225a0168f875c9350c0861422d47c
Frostack <soulburn007@gmail.com>
2020-08-19 20:57:21 +0430

    💄 add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 import store from './store';
 import App from './components/App';
 
