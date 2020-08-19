8fc458911320f3fc768bfebb7a7489c235751b0f
Frostack <soulburn007@gmail.com>
2020-08-19 20:58:48 +0430

    🧱 add App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './Header';
+import BeerScreen from './BeerScreen/BeerScreen';
+import CartScreen from './CartScreen/CartScreen';
+import BeerModal from './UI/Modal/BeerModal';
+import ErrorModal from './UI/Modal/ErrorModal';
 
 function App() {
   return (
-    <>
-      <CssBaseline />
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Redirect exact from="/" to="/ " />
+        <Route exact path="/ " component={BeerScreen} />
+        <Route exact path="/pizzas" component={BeerScreen} />
+        <Route exact path="/steaks" component={BeerScreen} />
+        <Route exact path="/favorites" component={BeerScreen} />
+        <Route exact path="/cart" component={CartScreen} />
+      </Switch>
+      <BeerModal />
+      <ErrorModal />
+    </Router>
   );
 }
 
