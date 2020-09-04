79357826b3523edaf6c3a8907e91a8d7cf2cc187
Frostack <soulburn007@gmail.com>
2020-09-04 12:39:54 +0430

    ✨ DisableableButton now passes className and block props

--- src/components/common/DisableableButton.js
+++ src/components/common/DisableableButton.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { BsBagFill } from 'react-icons/bs'
 
-function DisableableButton({ disabled, handleClick, activeText, disabledText }) {
+function DisableableButton({ disabled, handleClick, activeText, disabledText, className, block }) {
   return (
     <Button
-      className="d-flex align-items-center ml-5"
+      className={className}
       variant={disabled ? 'secondary' : 'primary'}
       disabled={disabled}
       onClick={handleClick}
+      block={block}
     >
       {disabled ? disabledText : activeText} <BsBagFill size={18} className="ml-3" />
     </Button>
