6931bae93dd90338c621817a9d1ca18bc4506ed0
Frostack <soulburn007@gmail.com>
2020-09-04 12:40:43 +0430

    🔥 remove extra space

--- src/components/common/PriceTag.js
+++ src/components/common/PriceTag.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PriceTag({ price }) {
   const renderedPrice = price || 0
-  return <div className="text-success font-weight-bolder">{renderedPrice.toFixed(2)} $</div>
+  return <div className="text-success font-weight-bolder">{renderedPrice.toFixed(2)}$</div>
 }
 
 export default PriceTag
