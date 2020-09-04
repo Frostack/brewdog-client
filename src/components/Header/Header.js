00cbd6780f8f7a539361e344fa68ea7ed65177fa
Frostack <soulburn007@gmail.com>
2020-09-04 12:41:41 +0430

    ✨ Header is responsive now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,35 +7,39 @@ import HeaderLink from './HeaderLink'
 
 function Header() {
   return (
-    <Navbar bg="light" className="px-4 fixed-top">
+    <Navbar collapseOnSelect expand="lg" bg="light" className="px-4 fixed-top">
       <Navbar.Brand className="d-flex align-items-center text-warning">
         <BsStarFill className="mr-1" size={20} />
         Brewdog
       </Navbar.Brand>
 
-      <Nav defaultActiveKey="/ " className="ml-auto">
-        <HeaderLink to="/ " className="mx-4">
-          All
-        </HeaderLink>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-        <HeaderLink to="/pizzas" className="mx-4">
-          With Pizza
-        </HeaderLink>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav defaultActiveKey="/ " className="ml-auto">
+          <HeaderLink to="/ " className="mx-4">
+            All
+          </HeaderLink>
 
-        <HeaderLink to="/steaks" className="mx-4">
-          With Steak
-        </HeaderLink>
+          <HeaderLink to="/pizzas" className="mx-4">
+            With Pizza
+          </HeaderLink>
 
-        <HeaderLink to="/favorites" className="text-danger mx-4">
-          Favorites
-          <BsHeartFill className="ml-1" size={20} />
-        </HeaderLink>
+          <HeaderLink to="/steaks" className="mx-4">
+            With Steak
+          </HeaderLink>
 
-        <HeaderLink to="/cart" className="d-flex align-items-center text-success mx-4">
-          Cart
-          <BsBagFill className="ml-1" size={20} />
-        </HeaderLink>
-      </Nav>
+          <HeaderLink to="/favorites" className="text-danger mx-4">
+            Favorites
+            <BsHeartFill className="ml-1" size={20} />
+          </HeaderLink>
+
+          <HeaderLink to="/cart" className="d-flex align-items-center text-success mx-4">
+            Cart
+            <BsBagFill className="ml-1" size={20} />
+          </HeaderLink>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
